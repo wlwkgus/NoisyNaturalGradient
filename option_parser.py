50bd0ffa6b8f0774307ff40edb3d34aa23e69c88
Steven H. Wang <wang.steven.h@gmail.com>
2018-08-31 17:55:11 -0700

    Add --cpu flag
    `--gpu-ids=''` results in an parsing error, since the code will try
    to cast the empty string into an int.

--- option_parser.py
+++ option_parser.py
@@ -12,12 +12,15 @@ class OptionParser(object):
 
     def parse_args(self):
         opt = self.parser.parse_args()
-        str_ids = str(opt.gpu_ids)
-        opt.gpu_ids = []
-        for str_id in str_ids.split(','):
-            opt.gpu_ids.append(int(str_id))
-        if len(opt.gpu_ids) > 0:
+
+        if not opt.cpu:
+            str_ids = str(opt.gpu_ids)
+            opt.gpu_ids = []
+            for str_id in str_ids.split(','):
+                opt.gpu_ids.append(int(str_id))
             torch.cuda.set_device(opt.gpu_ids[0])
+        else:
+            opt.gpu_ids = []
 
         args = vars(opt)
 
@@ -41,6 +44,7 @@ class OptionParser(object):
         self.parser.add_argument('--batch_size', type=int, default=100, help='batch size')
         self.parser.add_argument('--num_preprocess_workers', type=int, default=2, help='num preprocess workers')
         self.parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids ex) 0,1,2')
+        self.parser.add_argument('--cpu', action="store_true", help='Train on CPU. Overrides `gpu_ids` option.')
         self.parser.add_argument('--ckpt_dir', type=str, default='./ckpt/', help='checkpoint dir')
         self.parser.add_argument('--model', type=str, default='FFGModel', help='name of model')
         self.parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
